532942fa2c3ec6e4dc1b01ddda1a8129446f1654
iTucker-Rasbury <70918983+TuckerRasbury@users.noreply.github.com>
2021-02-04 00:31:58 -0800

    Module 11, challenge deliverable 1 half complete

--- static/js/app.js
+++ static/js/app.js
@@ -36,18 +36,23 @@ let filters = {};
 function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
-    let changedElement = d3.select(this).select("input"); //input fills let filters={};
+    let changedElement = d3.select(this).select("input") //input refers to html line.
 
-    // 4b. Save the value that was changed as a variable, from 4a.
-    let elementValue = changedElement.property("value");
+    // 4b. Save the value that was changed as a variable, ie. from changedElement.
+    let elementValue = changedElement.property("value")
 
-    // 4c. Save the id of the filter that was changed as a variable.
+    // 4c. Save the id of the filter that was changed as a variable, ie. from changedElement.
+    let elementId = changedElement.attr("id")
 
-  
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
+    if (elementValue) {
+      filters[elementId] = elementValue
+    }
+    else {
+      delete filters[elementId]
+    }
  
-  
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
   
